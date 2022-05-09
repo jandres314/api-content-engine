af90e2e14062149305fd18903b902bd85bae40a2
josori <josorio@pratechgroup.com>
2022-05-09 09:04:04 -0500

    permisos y autorizacion

--- src/main/java/com/api/ce/permisosyautorizacion/PermisosAutorizacion.java
+++ src/main/java/com/api/ce/permisosyautorizacion/PermisosAutorizacion.java
@@ -1,14 +1,41 @@
 package com.api.ce.permisosyautorizacion;
 
+import com.filenet.api.collection.AccessPermissionList;
+import com.filenet.api.constants.AccessLevel;
+import com.filenet.api.constants.AccessType;
+import com.filenet.api.constants.RefreshMode;
+import com.filenet.api.core.Document;
+import com.filenet.api.core.Factory;
 import com.filenet.api.core.ObjectStore;
+import com.filenet.api.security.AccessPermission;
+import com.filenet.api.util.Id;
 
+@SuppressWarnings("deprecation")
 public class PermisosAutorizacion {
 
-	public boolean asignarPermisos(ObjectStore os) {
-		return false;
-	}
+	@SuppressWarnings({"unchecked" })
+	public boolean asignarPermisosFullControlUsuariosAutenticados(ObjectStore os, String idDocument) {
 
-	public boolean retirarPermisos(ObjectStore os) {
+		Document doc = Factory.Document.fetchInstance(os, new Id(idDocument), null);
+
+		// Create a new permissions list.
+		AccessPermissionList apl = Factory.AccessPermission.createList();
+
+		// create a new access permission object
+		AccessPermission ap = Factory.AccessPermission.createInstance();
+
+		ap.set_GranteeName("#AUTHENTICATED-USERS");
+		ap.set_AccessType(AccessType.ALLOW);
+		ap.set_AccessMask(AccessLevel.FULL_CONTROL_ANNOTATION_AS_INT);
+
+		// Add the permissions to the list.
+		apl.add(ap);
+
+		// set apl
+		doc.set_Permissions(apl);
+
+		doc.save(RefreshMode.NO_REFRESH);
+
 		return true;
 	}
 
--- src/test/java/com/api/ce/permisosyautorizacion/PermisosAutorizacionTest.java
+++ src/test/java/com/api/ce/permisosyautorizacion/PermisosAutorizacionTest.java
@@ -1,5 +1,7 @@
 package com.api.ce.permisosyautorizacion;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import com.api.ce.conexiones.ConexionOs;
@@ -14,11 +16,9 @@ public class PermisosAutorizacionTest {
 	@Test
 	public void asingarPermisos() {
 		ObjectStore os = conexionOs.obtenerConexionObjectStore(FactoryDatosConexion.obtenerDatosConexion());
+		String idDocumento = "{F0E69980-0000-C41D-979C-CEA83B0D4A5F}";
+		boolean resultado = permisos.asignarPermisosFullControlUsuariosAutenticados(os, idDocumento);
+		assertTrue(resultado);
 	}
 	
-	@Test
-	public void retirarPermisos() {
-		ObjectStore os = conexionOs.obtenerConexionObjectStore(FactoryDatosConexion.obtenerDatosConexion());
-	}
-
 }
