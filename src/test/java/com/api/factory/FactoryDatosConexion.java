bb8280eccfd8413ff034424155acd339322ec35a
josori <josorio@pratechgroup.com>
2022-05-11 17:13:25 -0500

    se limpian datos de prueba

--- src/test/java/com/api/factory/FactoryDatosConexion.java
+++ src/test/java/com/api/factory/FactoryDatosConexion.java
@@ -9,11 +9,11 @@ public class FactoryDatosConexion {
 	
 	public static DatosConexionOs obtenerDatosConexion() {
 		DatosConexionOs datos = new DatosConexionOs();
-		datos.setContrasena("Pratech2016$");
-		datos.setUrl("http://10.94.84.167:9080/wsi/FNCEWS40MTOM");
-		datos.setDominio("P8DomainDev");
-		datos.setObjectStore("OSDEV");
-		datos.setUsuario("p8admindllo");
+		datos.setContrasena("");
+		datos.setUrl("");
+		datos.setDominio("");
+		datos.setObjectStore("");
+		datos.setUsuario("");
 		datos.setConexionWsi(true);
 		return datos;
 	}
@@ -21,10 +21,10 @@ public class FactoryDatosConexion {
 	public static DatosConexionPe obtenerDatosConexionPe() {
 		
 		DatosConexionPe datosConexion = new DatosConexionPe();
-		datosConexion.setContrasena("Pratech2016$");
-		datosConexion.setPuntoConexion("PECPDEV");
-		datosConexion.setUrl("http://10.94.84.167:9080/wsi/FNCEWS40MTOM");
-		datosConexion.setUsuario("p8admindllo");
+		datosConexion.setContrasena("");
+		datosConexion.setPuntoConexion("");
+		datosConexion.setUrl("");
+		datosConexion.setUsuario("");
 		return datosConexion;
 	}
 
